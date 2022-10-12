7f767b5868c1a513a7d1ed4c6f4f4611e9f93b89
Om Khadka <om.khadka.101@icloud.com>
2022-10-12 19:16:05 -0400

    10.12.22 - 7:15pm
    Added functionality to the menu & main scene buttons by allowing to transition between the 2 & being able to exit the application.

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,24 +1,44 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR // Adds the UnityEditor namespace if in the Unity editor.
+using UnityEditor;
+#endif
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
 [DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
+    /* Properties */
     public ColorPicker ColorPicker;
 
-    public void NewColorSelected(Color color)
-    {
-        // add code here to handle when a color is selected
-    }
-    
+
+    /* Methods */
     private void Start()
     {
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+    }
+
+    public void NewColorSelected(Color color)
+    {
+        // add code here to handle when a color is selected
+    }
+
+    // Transitions to the main scene.
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Quits the game.
+    public void Exit()
+    {
+#if UNITY_EDITOR // Quits the game in the Unity editor.
+        EditorApplication.ExitPlaymode();
+#else // Quites the game in any built version.
+        Application.Quit();
+#endif
     }
 }
